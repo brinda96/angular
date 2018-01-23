2c2d47776016f3275b97146c4d49a14605c6222a
brinda96 <brindagiridharan@gmail.com>
2018-01-23 04:30:50 -0500

    Add files via upload

--- hero.service.ts
+++ hero.service.ts
@@ -11,7 +11,11 @@ import { MessageService } from './message.service';
 export class HeroService {
 
   constructor(private messageService: MessageService) { }
-
+  getHero(id: number): Observable<Hero> {
+    // Todo: send the message _after_ fetching the hero
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
+  }
   getHeroes(): Observable<Hero[]> {
     // Todo: send the message _after_ fetching the heroes
     this.messageService.add('HeroService: fetched heroes');
